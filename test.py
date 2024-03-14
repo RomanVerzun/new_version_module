3de1668ec46e845f2ce17c0f154b930d6011f2e4
trainer <romanverzun2019@gmail.com>
2024-03-14 17:54:13 +0200

    save

--- test.py
+++ test.py
@@ -1,38 +1,50 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QSpinBox
-from PyQt5.QtCore import QTimer
+class Root:
+    def ping(self):
+        print(f'{self}.ping() in Root')
+    
+    def pong(self):
+        print(f'{self}.pong() in Root')
+    
+    def __repr__(self):
+        cls_name = type(self).__name__
+        return f'<instance of {cls_name}>'
 
-class MyWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-        
-    def initUI(self):
-        self.layout = QVBoxLayout()
-        
-        # Создаем QSpinBox
-        self.spinBox = QSpinBox(self)
-        self.spinBox.setMinimum(0) # Минимальное значение
-        self.spinBox.setMaximum(100) # Максимальное значение
-        
-        self.layout.addWidget(self.spinBox)
-        self.setLayout(self.layout)
-        
-        # Настройка QTimer
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.updateSpinBox)
-        self.timer.start(50) # Задержка в миллисекундах
-        
-    def updateSpinBox(self):
-        # Увеличиваем значение на 1, сбрасываем, если достигнуто максимальное
-        currentValue = self.spinBox.value()
-        if currentValue < self.spinBox.maximum():
-            self.spinBox.setValue(currentValue + 1)
-        else:
-            self.spinBox.setValue(self.spinBox.minimum())
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MyWindow()
-    window.show()
-    sys.exit(app.exec_())
+class A(Root):
+    def ping(self):
+        print(f'{self}.ping() in A')
+        super().ping()
+    
+    def pong(self):
+        print(f'{self}.pong() in A')
+        super().pong()
+
+
+class B(Root):
+    def ping(self):
+        print(f'{self}.ping() in B')
+        super().ping()
+    
+    def pong(self):
+        print(f'{self}.pong() in B')
+
+
+class Leaf(A, B):
+    def ping(self):
+        print(f'{self}.ping() in Leaf')
+        super().ping()
+
+
+class U():
+    def ping(self):
+        print(f'{self}.ping() in LeafUA')
+        #super().ping()
+
+
+class LeafUA(U, A):
+    def ping(self):
+        print(f'{self}.ping() in LeafUA')
+        super().ping()
+
+import tkinter
+print(dir(tkinter.Button))
